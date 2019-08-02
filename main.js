93546c169a781ac5d3adb7f037ee14a2ce92b9fc
zhoujun <197652334@qq.com>
2019-08-02 10:42:35 +0800

    22

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var fs=require('fs');
 let mainWindow
 
 function createWindow () {
-//创建浏览器窗口,宽高自定义具体大小你开心就好
+
 mainWindow = new BrowserWindow({width: 1200, height: 1000,  webPreferences: {
   javascript: true,
   plugins: true,
